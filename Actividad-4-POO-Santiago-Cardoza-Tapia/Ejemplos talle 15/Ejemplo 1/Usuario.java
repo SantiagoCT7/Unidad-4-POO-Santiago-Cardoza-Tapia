0064d2e6844b619f1d4fe7865c527a4c0d05daff
SantiagoCT7 <scardonat@unicartagena.edu.co>
2025-07-08 10:28:31 -0500

    Delete Actividad-4-POO-Santiago-Cardoza-Tapia/Ejemplos talle 15 directory

--- Actividad-4-POO-Santiago-Cardoza-Tapia/Ejemplos talle 15/Ejemplo 1/Usuario.java
+++
@@ -1,9 +0,0 @@
-public class Usuario{
-   private String nombre;
-   private String contrasena;
-   
-   public Usuario(String nombre, String contrasena){
-    this.nombre = nombre;
-    this.contrasena = contrasena;
-   }
-}
--- Actividad-4-POO-Santiago-Cardoza-Tapia/Ejemplos talle 15/SMSService.java
+++
@@ -1,6 +0,0 @@
-public class SMSService {
-    public void enviarSMS(String numero, String mensaje) {
-        System.out.println("Enviando SMS a: " + numero);
-        System.out.println("Mensaje: " + mensaje);
-    }
-}
