e50bd5828455b262d400b40c1542ef681f989b8a
SantiagoCT7 <scardonat@unicartagena.edu.co>
2025-07-08 10:21:55 -0500

    Delete Ejercicios taller 1 directory

--- Ejercicios taller 1/Ejercicio 2/Persona9.java
+++
@@ -1,9 +0,0 @@
-public class Persona9 implements Hablador, Trabajador{
-    public void hablar(){
-        System.out.println("Prueba habla.");
-    }
-
-    public void trabajar(){
-        System.out.println("Chamba.");
-    }
-}
