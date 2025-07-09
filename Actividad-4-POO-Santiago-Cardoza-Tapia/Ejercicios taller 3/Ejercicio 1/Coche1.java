79adbd5cab88071f566612a7e547a60499433b6b
SantiagoCT7 <scardonat@unicartagena.edu.co>
2025-07-08 19:23:50 -0500

    Add files via upload

---
+++ Actividad-4-POO-Santiago-Cardoza-Tapia/Ejercicios taller 3/Ejercicio 1/Coche1.java
@@ -0,0 +1,29 @@
+public class Coche1{
+    private String marca;
+    private String modelo;
+    private int velocidadMaxima;
+
+    public Coche1(String marca, String modelo, int velocidadMaxima){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void acelerar(int incremento){
+        if (incremento > 0) {
+            velocidadMaxima += incremento;
+        }
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public int getVelocidadMaxima(){
+        return velocidadMaxima;
+    }
+}
